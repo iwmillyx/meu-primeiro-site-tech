f371728851b2df026002d808df28ab8db84e172a
Emilly <emillyts@dcc.ufrj.br>
2025-07-28 18:17:16 -0300

    Adicionando favicon, atualizando CSS e melhorando validação do formulário

--- js/script.js
+++ js/script.js
@@ -1,10 +1,15 @@
-document.getElementById("botaoEnviar").addEventListener("click", validaFormulario )
+document.getElementById("botaoEnviar").addEventListener("click", validaFormulario);
 
-function validaFormulario(){
-    if(document.getElementById("nome").value != "" && document.getElementById("email").value != "" && document.getElementById("telefone").value != ""){
-        alert("Prontinho! Você receberá as novidades por email.")
-    }else{
-        alert("Por favor, preencha os campos nome, email e telefone!")
+function validaFormulario(event) {
+    event.preventDefault();
+
+    const nome = document.getElementById("nome").value;
+    const email = document.getElementById("email").value;
+    const telefone = document.getElementById("telefone").value;
+
+    if (nome !== "" && email !== "" && telefone !== "") {
+        alert("Prontinho! Você receberá as novidades por email.");
+    } else {
+        alert("Por favor, preencha os campos nome, email e telefone!");
     }
 }
-
